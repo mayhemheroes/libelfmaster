d79d4a182ddcc3e7699e48b7b8a923e5f0517268
xansec <76011430+xansec@users.noreply.github.com>
2023-02-28 15:18:50 -0500

    Merge pull request #2 from rnshah9/master
    Phase II submission improvement

---
+++ mayhem/fuzz_elf_open_object.cpp
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <climits>
+#include <fuzzer/FuzzedDataProvider.h>
+
+extern "C"
+{
+#include "libelfmaster.h"
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
+{
+    FuzzedDataProvider provider(data, size);
+    std::string str = provider.ConsumeRandomLengthString(1000);
+    uint64_t flags = provider.ConsumeIntegral<uint64_t>();
+    elfobj_t elfobj;
+    elf_error_t elf_error;
+
+    elf_open_object(str.c_str(), &elfobj, flags, &elf_error);
+
+    return 0;
+}
